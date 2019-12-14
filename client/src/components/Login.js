39cb8d2c79bd735f4283f691f6b10ab72da4108d
Daniel Cruz <d.cruz.489@hotmail.com>
2019-12-13 19:47:47 -0700

    added api and login components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import api from '../utils/api';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = (props) => {
+  const [error, setError] = useState()
+  const [status, setStatus] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleChange = (event) => {
+    setStatus({
+      ...status,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    api().post("/api/login", status)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/account')
+      })
+      .catch(err => {
+        setError(err.res)
+      })
+  }
+
   return (
-    <>
+    <>  
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+      <form onSubmit={handleSubmit}>
+        {error && <div className='error'>{error}</div>}
+        <input type='text' name='username' placeholder="Username" value={status.username} onChange={handleChange} />
+        <input type='password' name='password' placeholder="Password" value={status.password} onChange={handleChange} />
+        <button type='submit'>Login</button>
+      </form>
+    </>	    
+  )
+}
 
 export default Login;
